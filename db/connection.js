7b17914d1d64d183cb4865696d7b724f18b0925e
Jon <churchjg283@gmail.com>
2020-11-10 15:25:36 -0500

    first commit

---
+++ db/connection.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+
+//mongoose.connect(mongoURI, { useNewUrlParser: true },{ useUnifiedTopology: true } );
+
+module.exports = mongoose
---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const cors = require("cors");
+
+// const foodInfo = require('../models/foodinfo');
+app.use(cors());
+app.use(parser.json());
